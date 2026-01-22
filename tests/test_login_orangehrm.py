e520d198d4fc9bf7bccec0a43e72ab4f4f532307
Daniru Dayarathna <168005576+danirudayarathna@users.noreply.github.com>
2026-01-22 17:26:52 +0530

    Update OrangeHRM login test with credentials
    Enhanced the OrangeHRM login test to fill in the username and password fields and click the login button. Updated the HTML report to reflect the new test execution.

--- tests/test_login_orangehrm.py
+++ tests/test_login_orangehrm.py
@@ -2,4 +2,6 @@ from playwright.sync_api import Page, expect
 
 def test_exmaple(page: Page) -> None:
     page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    page.get_by_role("Username" )
+    page.get_by_role("textbox", name="Username").fill("Admin")
+    page.get_by_role("textbox", name="password").fill("admin123")
+    page.get_by_role("button", name="Login").click()
