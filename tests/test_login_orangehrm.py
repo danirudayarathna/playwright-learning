84287b865fdfdc59973d609e4982e4ac2d8ee0f8
Daniru Dayarathna <168005576+danirudayarathna@users.noreply.github.com>
2026-01-23 17:32:38 +0530

    Add assertion for page title after login in test
    Enhanced the OrangeHRM login test to click the first sidebar item after login and assert that the page title is 'OrangeHRM'. This helps verify successful navigation and correct page load after login.

--- tests/test_login_orangehrm.py
+++ tests/test_login_orangehrm.py
@@ -5,3 +5,7 @@ def test_exmaple(page: Page) -> None:
     page.get_by_role("textbox", name="Username").fill("Admin")
     page.get_by_role("textbox", name="password").fill("admin123")
     page.get_by_role("button", name="Login").click()
+
+    page.locator("xpath=//aside[@class='oxd-sidepanel']//li[1]").click()
+    assert page.title() == "OrangeHRM"
+
